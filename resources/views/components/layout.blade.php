ab3c7e2a26fa41ef241c367253e2c8c7dfd302d9
Guentalina Allegretti <guentalina.allegretti@gmail.com>
2024-08-12 14:57:31 +0200

    [FIX]masthead

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="@yield('meta_keywords')">
     <meta name="description" content="@yield('meta_description')">
+    <meta property="og:image" content="https://aridossodeisassicasavacanzematera.it/img/logo.png" />
 
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
 
